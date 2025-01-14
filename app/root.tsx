2f672358b0cfae35b09afaa9b7db5e992bdd7b17
Herdian <herdiansc@gmail.com>
2025-01-14 20:55:47 +0700

    add google analytics

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,29 @@ export default function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+
+        <script
+          type="module" 
+          dangerouslySetInnerHTML={{
+            __html: `
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAvHJTVctjLml0Nm0iGECs2uNFUTIbOBGk",
+    authDomain: "hitungperingkatfifa.firebaseapp.com",
+    projectId: "hitungperingkatfifa",
+    storageBucket: "hitungperingkatfifa.firebasestorage.app",
+    messagingSenderId: "576900346065",
+    appId: "1:576900346065:web:b21905dd6bdc74e4fa55f0",
+    measurementId: "G-RRLTK0DVCK"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+    `,
+          }}
+        />
       </head>
       <body>
         <Outlet />
