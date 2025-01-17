4c9ba5b95389beadf60eae252ccd227dfce41ed3
Herdian <herdiansc@gmail.com>
2025-01-17 21:28:42 +0700

    add btn download card

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,9 @@ import daisyTheme from "daisyui/src/theming/themes"
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ['"Open Sans", sans-serif'] // Example with a custom font
+    },
     container: {
       center: true,
       padding: "16px",
