0cc146f7adbf2b3bd41cf2b12fb7247e6d3c3487
Herdian <herdiansc@gmail.com>
2025-01-21 20:32:58 +0700

    fix article meta page

--- app/routes/_public.article.$slug.tsx
+++ app/routes/_public.article.$slug.tsx
@@ -1,35 +1,68 @@
 import type { LoaderFunction, MetaFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
+import React from 'react';
 import PointCalculationMethod from '~/components/article/point-calculation-method';
 
-export const loader: LoaderFunction = ({params}) => {
-  return params;
+type ArticleDetail = {
+  component: string; //React.ReactNode; 
+  title: string; 
+  description: string;
+};
+type ArticleMaps = {
+  [key: string]: ArticleDetail;
+};
+type ArticleComponents = {
+  [key: string]: React.ReactNode;
+};
+const articleMaps: ArticleMaps = {
+  'cara-perhitungan-poin-peringkat-fifa': {
+    component: 'PointCalculationMethod', //<PointCalculationMethod />,
+    title: 'Cara Perhitungan Poin dan Peringkat FIFA',
+    description: 'Bagaimana Cara Menghitung Peringkat FIFA, Penjelasan Lengkap, dan Contoh Perhitungannya',
+  }
 };
 
-export const meta: MetaFunction = () => {
+const articleComponents: ArticleComponents = {
+  'PointCalculationMethod': <PointCalculationMethod />,
+};
+
+const getArticleMeta = (slug: string | undefined) => {
+  if (!slug) {
+    return false;
+  }
+  return articleMaps[slug];
+};
+
+const getArticleComponent = (component: string) => {
+  return articleComponents[component];
+};
+
+export const loader: LoaderFunction = ({params}) => {
+  const article = getArticleMeta(params.slug);
+  if (!article) {
+    throw new Response(null, {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+  return article;
+};
+
+export const meta: MetaFunction<typeof loader> = ({data}) => {
   return [
-    { title: 'Artikel' },
+    { title: data.title + '| Artikel' },
     {
       name: 'description',
-      content:
-        'Pilih negara yang akan bertanding dan hitung perolehan poin peringkat fifa setelahnya jika salah satu menang atau draw.',
+      content: data.description,
     },
   ];
 };
 
-const articleMaps = {
-  'cara-perhitungan-poin-peringkat-fifa': <PointCalculationMethod />
-};
-
-const getArticleComponent = (slug) => {
-  return articleMaps[slug];
-};
-
 export default function Index() {
-  const { slug } = useLoaderData();
+  const article = useLoaderData<typeof loader>();
   return (
     <>
-      {getArticleComponent(slug)}
+      {getArticleComponent(article.component)}
     </>
   );
 }
